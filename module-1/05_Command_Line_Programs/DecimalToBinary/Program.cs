3ed77f7becc154bf65f3b628c955517a2af426bd
Grant Payne <afroman7600@yahoo.com>
2019-01-19 18:58:34 -0500

    Completed Fibonacci and Decimal to Binary (Fibonacci recursive incomplete)

--- module-1/05_Command_Line_Programs/DecimalToBinary/Program.cs
+++ module-1/05_Command_Line_Programs/DecimalToBinary/Program.cs
@@ -6,7 +6,25 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter in a series of decimal values (separated by spaces):");
+            string userInput = Console.ReadLine();
+            string[] userValues = userInput.Split(" ");
+            for (int i = 0; i < userValues.Length; i++)
+            {
+                Console.WriteLine(userValues[i] + " in binary is " + BinaryConvert(int.Parse(userValues[i])));
+            }
+            Console.ReadLine();
+        }
+        static String BinaryConvert(int input)
+        {
+            string result = "";
+            while (input >= 1)
+            {
+                result = (input % 2).ToString() + result;
+                input /= 2;
+
+            }
+            return result;
         }
     }
 }
--- module-1/05_Command_Line_Programs/Fibonacci/Program.cs
+++ module-1/05_Command_Line_Programs/Fibonacci/Program.cs
@@ -6,7 +6,70 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter the Fibonacci number: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            while (inputNumber >= 0)
+            {
+            Fibonacci(inputNumber);
+            Console.WriteLine();
+            inputNumber = int.Parse(Console.ReadLine());
+            }
+
         }
+
+        static void Fibonacci(int input)
+        {
+            int num1 = 0;
+            int num2 = 1;
+
+            if (input == 0)
+            {
+                Console.WriteLine(num1);
+            }
+            /*
+            else if (input == 1)
+            {
+                Console.WriteLine(num1 + ", " + num2);
+            }
+            */
+            else
+            {
+                int nextFib = num1 + num2;
+                Console.Write(num1 + ", " + num2);
+                while (nextFib <= input)
+                {
+                    Console.Write(", " + nextFib);
+                    num1 = num2;
+                    num2 = nextFib;
+                    nextFib = num1 + num2;
+                }
+            }
+            Console.WriteLine();
+            
+        }
+
+        /* Recursive Method///////////////////////////////////////////////////////////////////////////////////
+        static int Fibonacci(int input)
+        {
+            int result = 0;
+
+            if (input <= 0)
+            {
+                return 0;
+            }
+            else if (input == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                result += Fibonacci(input - 2) + Fibonacci(input - 1);
+            }
+
+
+            return result;
+        }
+        */
+
     }
 }
