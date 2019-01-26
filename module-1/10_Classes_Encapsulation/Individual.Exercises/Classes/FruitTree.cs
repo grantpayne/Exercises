dfa80dc65e18c03835d19e470a7705363a44c01d
Grant Payne <afroman7600@yahoo.com>
2019-01-26 18:04:00 -0500

    Completed 2 problems in exercise 10 classes encapsulation successfully

--- module-1/10_Classes_Encapsulation/Individual.Exercises/Classes/FruitTree.cs
+++ module-1/10_Classes_Encapsulation/Individual.Exercises/Classes/FruitTree.cs
@@ -8,6 +8,38 @@ namespace Individual.Exercises.Classes
 {
     public class FruitTree
     {
-        
+        public string TypeOfFruit { get; }
+        private int piecesOfFruitLeft;
+        public int PiecesOfFruitLeft
+        {
+            get
+            {
+                return piecesOfFruitLeft;
+            }
+            private set
+            {
+                PiecesOfFruitLeft = piecesOfFruitLeft;
+            }
+
+           
+        }
+        public bool PickFruit(int numberOfPiecesToRemove)
+        {
+            if (piecesOfFruitLeft > 0)
+            {
+                piecesOfFruitLeft -= numberOfPiecesToRemove;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public FruitTree(string typeOfFruit, int startingPiecesOfFruit)
+        {
+            TypeOfFruit = typeOfFruit;
+            piecesOfFruitLeft = startingPiecesOfFruit;
+        }
     }
 }
--- module-1/10_Classes_Encapsulation/Individual.Exercises/Classes/HomeworkAssignment.cs
+++ module-1/10_Classes_Encapsulation/Individual.Exercises/Classes/HomeworkAssignment.cs
@@ -8,8 +8,72 @@ namespace Individual.Exercises.Classes
 {
     public class HomeworkAssignment
     {
-        
 
+        private int totalMarks;
+        public int TotalMarks
+        {
+            get
+            {
+                return totalMarks;
+            }
+            set
+            {
+                totalMarks = value;
+            }
+        }
+        private int possibleMarks;
+        public int PossibleMarks
+        {
+            get
+            {
+                return possibleMarks;
+            }
+        }
+        private string submitterName;
+        public string SubmitterName
+        {
+            get
+            {
+                return submitterName;
+            }
+        }
+        private string letterGrade = "";
+        public string LetterGrade
+        {
+            get
+            {
+                if ((double)TotalMarks / PossibleMarks > 0.90)
+                {
+                    letterGrade = "A";
+                }
+                else if ((double)TotalMarks / PossibleMarks >= 0.80)
+                {
+                    letterGrade = "B";
+
+                }
+                else if ((double)TotalMarks / PossibleMarks >= 0.70)
+                {
+                    letterGrade = "C";
+                }
+                else if ((double)TotalMarks / PossibleMarks >= 0.60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+
+                return letterGrade;
+            }
+        }
+
+        public HomeworkAssignment(int possibleMarks, string submitterName)
+        {
+            this.possibleMarks = possibleMarks;
+
+            this.submitterName = submitterName;
+        }
 
     }
 }
