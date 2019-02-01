b4ce6d39ea2b66bb3a2c87fd286fac9bbbc90c19
John Fulton <john@techelevator.com>
2019-02-01 16:53:38 -0500

    module-1/16_Extra_Optional_Exercises/

---
+++ module-1/16_Extra_Optional_Exercises/Exercises.Test/NumbersToWordsTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercises;
+
+namespace Exercises.Test
+{
+    [TestClass]
+    public class NumbersToWordsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
