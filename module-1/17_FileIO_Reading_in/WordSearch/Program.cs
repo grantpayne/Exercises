2397dbd26a6f6a024828d73d00276fc5fb020664
Grant Payne <afroman7600@yahoo.com>
2019-02-04 13:37:42 -0500

    Completed Word Search Part 2 in exercise set 17 sucessfully.

--- module-1/17_FileIO_Reading_in/WordSearch/Program.cs
+++ module-1/17_FileIO_Reading_in/WordSearch/Program.cs
@@ -11,6 +11,8 @@ namespace WordSearch
             //1. Ask the user for the search string
             Console.WriteLine("What is the search word you are looking for?");
             string searchWord = Console.ReadLine();
+            Console.WriteLine("Should the seatch be case sensitive (Y/N)");
+            char caseSensitive = char.Parse(Console.ReadLine());
             //2. Ask the user for the file path
             Console.WriteLine("What is fully qualified name of the file that should be searched?");
             string filepath = Console.ReadLine();
@@ -21,7 +23,7 @@ namespace WordSearch
             {
                 using (StreamReader sr = new StreamReader(filepath))
                 {
-            //4. Loop through each line in the file
+                    //4. Loop through each line in the file
                     while (!sr.EndOfStream)
                     {
                         string line = sr.ReadLine();
@@ -39,9 +41,21 @@ namespace WordSearch
             //5. If the line contains the search string, print it out along with its line number
             for (int i = 0; i < allLines.Count; i++)
             {
-                if (allLines[i].Contains(searchWord))
+                if (caseSensitive == 'N')
                 {
-                    Console.WriteLine($"{i + 1}) {allLines[i]}");
+
+                    if (allLines[i].ToUpper().Contains(searchWord.ToUpper()))
+                    {
+                        Console.WriteLine($"{i + 1}) {allLines[i]}");
+                    }
+
+                }
+                else
+                {
+                    if (allLines[i].Contains(searchWord))
+                    {
+                        Console.WriteLine($"{i + 1}) {allLines[i]}");
+                    }
                 }
             }
             Console.ReadLine();
