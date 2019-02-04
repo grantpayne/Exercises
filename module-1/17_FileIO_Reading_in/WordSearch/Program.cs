3349185157da552ec373362b77eb279c745ae087
Grant Payne <afroman7600@yahoo.com>
2019-02-04 13:24:31 -0500

    Completed Word Search Part 1 in exercise set 17 successfully.

--- module-1/17_FileIO_Reading_in/WordSearch/Program.cs
+++ module-1/17_FileIO_Reading_in/WordSearch/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace WordSearch
 {
@@ -7,10 +9,42 @@ namespace WordSearch
         static void Main(string[] args)
         {
             //1. Ask the user for the search string
+            Console.WriteLine("What is the search word you are looking for?");
+            string searchWord = Console.ReadLine();
             //2. Ask the user for the file path
+            Console.WriteLine("What is fully qualified name of the file that should be searched?");
+            string filepath = Console.ReadLine();
             //3. Open the file
+            List<string> allLines = new List<string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filepath))
+                {
             //4. Loop through each line in the file
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] inputArray = line.Split('\n');
+                        allLines.AddRange(inputArray);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(ex.Message);
+            }
             //5. If the line contains the search string, print it out along with its line number
+            for (int i = 0; i < allLines.Count; i++)
+            {
+                if (allLines[i].Contains(searchWord))
+                {
+                    Console.WriteLine($"{i + 1}) {allLines[i]}");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
