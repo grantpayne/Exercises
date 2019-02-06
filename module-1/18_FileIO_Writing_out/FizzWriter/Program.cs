e24eedf740cd3e9498ee08afe8d67a0923c0b4c5
Grant Payne <afroman7600@yahoo.com>
2019-02-05 20:27:16 -0500

    Completed FizzWriter portion of exercise 18 successfully.

--- module-1/18_FileIO_Writing_out/FizzWriter/Program.cs
+++ module-1/18_FileIO_Writing_out/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,45 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("FizzBuzz.txt", false))
+                {
+                    for (int i = 1; i <= 300; i++)
+                    {
+                        if (i % 3 == 0 && i % 5 == 0)
+                        {
+                            sw.WriteLine("FizzBuzz");
+                        }
+                        else if (i % 5 == 0 || i.ToString().Contains('5'))
+                        {
+                            sw.WriteLine("Buzz");
+                        }
+                        else if (i % 3 == 0 || i.ToString().Contains('3'))
+                        {
+                            sw.WriteLine("Fizz");
+                        }
+                        else
+                        {
+                            sw.WriteLine(i);
+                        }
+                    }
+                    
+                    
+                }
+                Console.WriteLine("FizzBuzz.txt has been created.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An exception occured");
+                Console.ReadLine();
+
+            }
+
+            
+                
         }
     }
 }
