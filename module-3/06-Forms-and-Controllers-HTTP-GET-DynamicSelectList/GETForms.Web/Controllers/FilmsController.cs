4a7559815a162b780b8dceb20919e3cdce394aaa
Andrew Frank <andrew@techelevator.com>
2019-03-06 12:29:59 -0500

    Add dynamic select list example for genres

---
+++ module-3/06-Forms-and-Controllers-HTTP-GET-DynamicSelectList/GETForms.Web/Controllers/FilmsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GETForms.Web.DAL;
+using GETForms.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GETForms.Web.Controllers
+{
+    public class FilmsController : Controller
+    {
+        private readonly IFilmDAL _filmDal;
+
+        public FilmsController(IFilmDAL filmDal)
+        {
+            _filmDal = filmDal;
+        }
+
+        /// <summary>
+        /// The request to display an empty search page.
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new FilmSearchViewModel();
+            var genreItems = new List<SelectListItem>();
+
+            foreach(string genre in _filmDal.GetGenres())
+            {
+                genreItems.Add(new SelectListItem { Text = genre });
+            }
+            model.Genres = genreItems;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string selectedGenre)
+        {
+            return Content(selectedGenre);
+        }
+
+        /// <summary>
+        /// Receives the search result request and goes to th database looking for the information.
+        /// </summary>
+        /// <param name="request">A request model that contains the search parameters.</param>
+        /// <returns></returns>
+        public ActionResult SearchResult(/*FilmSearch request */)
+        {
+            /* Call the DAL and pass the values as a model back to the View */
+            return null;
+        }
+    }
+}
---
+++ module-3/06-Forms-and-Controllers-HTTP-GET-DynamicSelectList/GETForms.Web/Models/FilmSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace GETForms.Web.Models
+{
+    public class FilmSearchViewModel
+    {
+        public string SelectedGenre { get; set; }
+        public IEnumerable<SelectListItem> Genres { get; set; }
+    }
+}
