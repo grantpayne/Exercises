10de14bba7c4dbfe69627e179900ebd240f2a189
Grant Payne <afroman7600@yahoo.com>
2019-03-19 10:46:16 -0400

    Completed first challenge problem in exercise 4-1

--- module-4/01_Introduction_to_JavaScript/challenge-exercises.js
+++ module-4/01_Introduction_to_JavaScript/challenge-exercises.js
@@ -1,1 +1,46 @@
-﻿
+﻿//1.
+function iqTest(str) {
+    let nums = [];
+    let odds = [];
+    let evens = [];
+    nums = str.split(' ');
+
+    if (nums.length == 0) {
+        return 0;
+    }
+    else {
+        odds = nums.filter((element) => {
+            return element % 2 == 1;
+        });
+        evens = nums.filter((element) => {
+            return element % 2 == 0;
+        });
+
+        if (odds.length > evens.length &&
+            evens.length == 1) {
+                return nums.indexOf(evens[0]) + 1;
+            }
+        else if (evens.length > odds.length &&
+            odds.length == 1) {
+                return nums.indexOf(odds[0]) + 1;
+            }
+        else if (odds.length == 0 || evens.length == 0) {
+            return 0;
+        }
+    }
+    
+}
+
+//2.
+function titleCase(str, ex) {
+    str = str.toLowerCase();
+    let phrase = [];
+    phrase = str.split(' ');
+    
+
+    ex = ex.toLowerCase();
+    let exeptions = [];
+    exceptions = str.split(' ');
+
+
+}
--- module-4/01_Introduction_to_JavaScript/exercises.js
+++ module-4/01_Introduction_to_JavaScript/exercises.js
@@ -128,3 +128,4 @@ function createObject() {
     };
     return object;
 }
+
