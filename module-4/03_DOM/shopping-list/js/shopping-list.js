be2b38664c1a91ec40ffc591aca9e0c0c72e7f96
Grant Payne <afroman7600@yahoo.com>
2019-03-21 07:54:29 -0400

    completed exercise 4-3 sucessfully

--- module-4/03_DOM/shopping-list/js/shopping-list.js
+++ module-4/03_DOM/shopping-list/js/shopping-list.js
@@ -1,17 +1,32 @@
 // add pageTitle
-
+const pageTitle = 'My Shopping List';
 // add groceries
+const groceries = ['bananas', 'coffee', 'mints', 'beer', 'peppers', 'cheese', 'soda', 'cookies', 'candy', 'onions'];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const title = document.getElementById('title');
+  title.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const list = document.getElementById('list');
+  for (let i = 0; i < groceries.length; i++) {
+    let li = document.createElement("li");
+    let item = document.createTextNode(groceries[i]);
+    li.appendChild(item);
+
+    let list = document.getElementById("list");
+    list.appendChild(li);
+  }
+  
+}
 
 /**
  * This function will be called wh4en the button is clicked. You will need to get a reference
